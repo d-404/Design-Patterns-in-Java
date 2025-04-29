e460f57326affd773d7610a2ed6bd13722dd886b
Souvik Das <souvik.404dgmail.com>
2025-04-29 23:06:05 +0530

    Minor change

--- singleton/src/main/java/Main.java
+++ singleton/src/main/java/Main.java
@@ -1,6 +1,3 @@
-// References  https://www.digitalocean.com/community/tutorials/java-singleton-design-pattern-best-practices-examples
-//https://www.geeksforgeeks.org/java-singleton-design-pattern-practices-examples/
-
 import java.io.*;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
@@ -8,10 +5,12 @@ import java.lang.reflect.InvocationTargetException;
 //Before jumping into pattern just explain what is lazy loading and eager loading
 // Mainly this class used to show violations using serializable and reflection.
 public class Main {
+
     public static void main(String[] args) throws IOException, ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException {
         //exampleSerialization();
-       exampleReflection();
+        exampleReflection();
     }
+
     private static void exampleSerialization() throws IOException, ClassNotFoundException {
         LazySingleton lazySingleton = LazySingleton.getInstance();
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("object.obj"));
@@ -33,15 +32,16 @@ public class Main {
         System.out.println("SerializableSingleton Object 1 :" + serializableSingleton.hashCode());
         System.out.println(" SerializableSingleton Object 2 :" + deserializedInstance.hashCode());
     }
+
     private static void exampleReflection() throws InvocationTargetException, InstantiationException, IllegalAccessException {
-       Constructor[] constructors = LazySingleton.class.getDeclaredConstructors();
-       //Knowing only one constructor taking it using index
+        Constructor[] constructors = LazySingleton.class.getDeclaredConstructors();
+        //Knowing only one constructor taking it using index
         Constructor constructor = constructors[0];
         constructor.setAccessible(true);
         LazySingleton lazySingleton = (LazySingleton) constructor.newInstance();
         LazySingleton instance = LazySingleton.getInstance();
-        System.out.println("Reflected hashcode singleton :"+lazySingleton.hashCode());
-        System.out.println("Singleton instance : "+ instance.hashCode());
+        System.out.println("Reflected hashcode singleton :" + lazySingleton.hashCode());
+        System.out.println("Singleton instance : " + instance.hashCode());
 
         //Solution to this is go by enum
         EnumSingleton.INSTANCE.doSomething();
